42f96f217610ddf683b711d96bd1b9e78603053a
ansxy <ansarfadillah17@gmail.com>
2023-11-11 15:20:52 +0800

    base project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+const port = process.env.PORT || 8000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
