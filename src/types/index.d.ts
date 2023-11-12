860362b20c85980b484660b33e9246d31eeab528
ansxy <ansarfadillah17@gmail.com>
2023-11-12 15:00:12 +0800

    style(type): Add new types for PopularCategory, RequestParams, and UserRegister.

---
+++ src/types/index.d.ts
@@ -0,0 +1,18 @@
+import { Request } from 'express'
+
+export interface PopularCategory {
+  categoryId: string
+  categoryName: string
+  enrollementCount: number
+}
+
+export interface RequestParams extends Request {
+  id: string
+  name: string
+}
+
+export interface UserRegister {
+  name: string
+  email: string
+  password: string
+}
