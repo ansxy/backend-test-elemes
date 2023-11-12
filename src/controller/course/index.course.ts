ad7cb5f9a57021b9425f2e33e7cad6d55a918274
ansxy <ansarfadillah17@gmail.com>
2023-11-13 01:42:19 +0800

    feat(course):Add CRUD functionality and enrollment feature to course controller
    add enrollment controller and fix create controller to add image name

--- src/controller/course/index.course.ts
+++ src/controller/course/index.course.ts
@@ -1,22 +1,46 @@
 import { course } from '@prisma/client'
 import { PrismaClientKnownRequestError, PrismaClientValidationError } from '@prisma/client/runtime/library'
-import { Request, Response } from 'express'
-import { createCourse, getAllCourse, getCourseById, searchCourse } from '../../services/course.service'
+import { Request, RequestParamHandler, Response } from 'express'
+import { UserRequest } from '../../middleware/verifyToken.middleware'
+import {
+  createCourse,
+  deleteCourse,
+  getAllCourse,
+  getCourseById,
+  searchCourse,
+  sortCourseByPrice,
+  updateCourse
+} from '../../services/course.service'
+import { enrollUser } from '../../services/enrollment.service'
+import { SortCourse } from '../../types/index'
+import { cloudUploud } from '../../utils/cloudinary'
+
+export interface CourseParams extends RequestParamHandler {
+  id: string
+}
 
 export const get = async (req: Request, res: Response) => {
   try {
-    if (req.params.id) {
-      const data = await getCourseById(req.params.id as string)
+    if (req.query.id) {
+      const data = await getCourseById(req.query.id as string)
       res.status(200).json({
         status: 'success',
         data: data
       })
     } else {
-      const data = await getAllCourse()
-      res.status(200).json({
-        status: 'success',
-        data: data
-      })
+      if (req.query.sort) {
+        const data = await sortCourseByPrice({ sort: req.query.sort } as SortCourse)
+        res.status(200).json({
+          status: 'success',
+          data: data
+        })
+      } else {
+        const data = await getAllCourse()
+        res.status(200).json({
+          status: 'success',
+          data: data
+        })
+      }
     }
   } catch (error) {
     if (error instanceof PrismaClientKnownRequestError) {
@@ -35,24 +59,26 @@ export const get = async (req: Request, res: Response) => {
 export const create = async (req: Request, res: Response) => {
   try {
     const { name, price, description, categoryId }: course = req.body
-    const data = await createCourse(name, price, description, categoryId)
+    const numericPrice = parseFloat(price.toString())
+    const data = await createCourse(name, numericPrice, description, categoryId, req.file?.filename as string)
+    const image = await cloudUploud(req.file?.path as string)
     res.status(201).json({
       status: 'success',
       data: {
         name: data.name,
         price: data.price.toString(),
         description: data.description,
-        categoryId: data.categoryId
+        categoryId: data.categoryId,
+        image: image.url
       }
     })
   } catch (error) {
     if (error instanceof PrismaClientValidationError) {
       res.status(400).json({
         status: 'fail',
-        message: `All Required Data Must Included`
+        message: error.message
       })
     } else {
-      console.log(error)
       res.status(500).json({
         error
       })
@@ -76,4 +102,60 @@ export const search = async (req: Request<object, object, object, { name?: strin
   }
 }
 
-module.exports = { get, create, search }
+export const update = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const { name, price, description }: Pick<course, 'id' | 'name' | 'price' | 'description'> = req.body
+    const data = await updateCourse({ id, name, price, description })
+    res.status(200).json({ status: 'success', data: data })
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      res.status(404).json({
+        status: 'fail',
+        message: error.meta?.cause
+      })
+    } else {
+      res.status(500).send(error)
+    }
+  }
+}
+export const del = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const data = await deleteCourse(id)
+    res.status(200).json({ status: 'success', data: data })
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      res.status(404).json({
+        status: 'fail',
+        message: error.meta?.cause
+      })
+    } else {
+      res.status(500).send(error)
+    }
+  }
+}
+
+export const enrollment = async (req: UserRequest, res: Response) => {
+  try {
+    if (req.user && typeof req.user === 'object' && 'userId' in req.user) {
+      const { id } = req.params
+      const data = await enrollUser({ id: req.user.userId }, id)
+      return res.status(201).json({
+        status: 'success',
+        data: data
+      })
+    }
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      res.status(409).json({
+        status: 'fail',
+        message: error.meta?.cause
+      })
+    } else {
+      res.status(500).send(error)
+    }
+  }
+}
+
+module.exports = { get, create, search, update, del, enrollment }
