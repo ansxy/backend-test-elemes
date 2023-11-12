04f1e5a5bd11b636b4700f18342f3de3369600c9
ansxy <ansarfadillah17@gmail.com>
2023-11-13 01:36:25 +0800

    feat (route):Add/update/delete routes
    add feat to delete course, enrollement course ,

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -2,13 +2,16 @@ import { simpleStatistic, softDeleteUser } from '../controller/admin/index.admin
 import * as category from '../controller/category/index.category'
 import * as course from '../controller/course/index.course'
 import { requireAdmin, verifyRefreshToken, verifyToken } from '../middleware/verifyToken.middleware'
+import { multerUploud } from '../utils/multer'
 import { createNewRouter } from '../utils/router'
 
 export const adminRoute = createNewRouter()
 
 adminRoute.put('/user/:id', [verifyToken, requireAdmin], softDeleteUser)
 adminRoute.post('/cetegory', [verifyToken, requireAdmin], category.create)
-adminRoute.post('/course', [verifyToken, requireAdmin], course.create)
+adminRoute.post('/course', [verifyToken, requireAdmin, multerUploud], course.create)
+adminRoute.put('/course/:id', [verifyToken, requireAdmin], course.update)
+adminRoute.delete('/course/:id', [verifyToken, requireAdmin], course.del)
 adminRoute.get('/simple-statistic', [verifyToken, requireAdmin, verifyRefreshToken], simpleStatistic)
 
 module.exports = { adminRoute }
--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -1,10 +1,11 @@
-import { get, search } from '../controller/course/index.course'
+import { enrollment, get, search } from '../controller/course/index.course'
 import { verifyToken } from '../middleware/verifyToken.middleware'
 import { createNewRouter } from '../utils/router'
 
 export const courseRoute = createNewRouter()
 
 courseRoute.get('/search/:name?', verifyToken, search)
-courseRoute.get('/:id?', verifyToken, get)
+courseRoute.get('/', verifyToken, get)
+courseRoute.post('/enrollment/:id', verifyToken, enrollment)
 
 module.exports = { courseRoute }
